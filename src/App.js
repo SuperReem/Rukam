1e1b17009f05f7f7c1b10dbfdeda359b2e9fef5a
Reem <reem66am@gmail.com>
2022-11-28 00:00:40 +0300

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit and save to reload.
+          Edit and save.
         </p>
         <a
           className="App-link"
