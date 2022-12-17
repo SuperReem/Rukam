4fcd41cf369eaed30661f19446037ea5132989e6
deemmf <88791711+deemmf@users.noreply.github.com>
2022-12-17 20:06:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@
 import './App.css';
 import Sidebar from './views/SideNavbar/SideNavbar';
 import Login from './views/Login/Login';
+import ResetPassword from './views/Login/ResetPassword';
+import ResetPass from './views/Login/ResetPass';
+
+
+
 function App() {
   return (
     <div className="App">
+
+
+
+      {/* <Login/> */}
+      {/* <ResetPassword/> */}
+      {/* <ResetPass/> */}
       <Sidebar/>
     </div>
   );
