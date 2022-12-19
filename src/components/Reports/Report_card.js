6c98c9f79d297c88ab5f67b8019e087a61eaacb1
Nailah-Alqahtani <NailahSMQ@gmail.com>
2022-12-19 05:12:59 +0300

    delete report pop-up

--- src/components/Reports/Report_card.js
+++ src/components/Reports/Report_card.js
@@ -35,9 +35,64 @@ function ReportCard({ status }) {
               >
                 <BsArrowUpLeft /> التفاصيل
               </Button>
-              <Button variant="secondary" size="md" id="delete-report-button">
+              <Button
+                variant="secondary"
+                size="md"
+                id="delete-report-button"
+                data-bs-toggle="modal"
+                data-bs-target="#myModal"
+              >
                 <FiTrash /> حذف البلاغ
               </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="modal" id="myModal">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="">
+                <div className="row align-items-center  justify-content-end mb-4 pt-2">
+                  <div className="col-6 p-0 ">
+                    <h4 className=" m-3">حذف البلاغ</h4>
+                  </div>
+                  <div className="col-2">
+                    <button
+                      data-bs-dismiss="modal"
+                      className="closebtn btn rounded"
+                    >
+                      &#x2715;
+                    </button>
+                  </div>
+                </div>
+                <div className="modal-body justify-content-center">
+                  <div className="row align-items-center  justify-content-center">
+                    <div className="col-8 progressbar  pb-4"></div>
+                    <div className="row align-items-center justify-content-between pb-4 me-5 pt-2">
+                      <div className="justify-content-center me-3 h4">
+                        هل انت متأكد من حذف هذا البلاغ؟
+                      </div>
+                    </div>
+                    <div className="row justify-content-start align-items-start">
+                      <div className="col-8 h5"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div></div>
+
+              <div className="modal-footer border border-0 justify-content-center">
+                <Button variant="secondary" size="md" id="delete-report-button">
+                  {" "}
+                  حذف{" "}
+                </Button>
+                <Button variant="secondary" size="md" id="details-button">
+                  {" "}
+                  إلغاء{" "}
+                </Button>
+              </div>
             </div>
           </div>
         </div>
--- src/views/Reports/UpdateStatus.js
+++ src/views/Reports/UpdateStatus.js
@@ -1,182 +1,171 @@
 import { render } from "@testing-library/react";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 import React, { useState } from "react";
-import MultiStepProgressBar from  "../../components/MultiStepProgressBar.js";
-import './ReportDetails.css';
-import Waste from '../../assets/images/waste.png';
+import MultiStepProgressBar from "../../components/MultiStepProgressBar.js";
+import "./ReportDetails.css";
+import Waste from "../../assets/images/waste.png";
 import { BsCalendar4 } from "react-icons/bs";
 
-
-function UpdateStatus(){
-   
+function UpdateStatus() {
   const [currentIndex, setCurrentIndex] = useState(1);
-  const [currentState, setCurrentState] = useState('تحديث الحالة الى قيد المراجعة ');
-  
-  
+  const [currentState, setCurrentState] = useState(
+    "تحديث الحالة الى قيد المراجعة "
+  );
+
   const check = () => {
-    if(currentIndex==3 && currentState==='تحديث الحالة الى مغلق'){
-    setCurrentState('حالة الطلب مغلق');}
-  }
+    if (currentIndex == 3 && currentState === "تحديث الحالة الى مغلق") {
+      setCurrentState("حالة الطلب مغلق");
+    }
+  };
   const update = () => {
-    if(currentIndex==1)
-   setCurrentIndex(2);
-   setCurrentState('تحديث الحالة الى مغلق');
-   if(currentIndex==2)
-   setCurrentIndex(3);
-   
-   if(currentIndex==3)
-   
-  console.log('dhbfh');
-  }
-  
-        return (
-            <div className="App">
-        <div className="row">
-          <div className="">
-            <h1 class="h5 text-end">البلاغات  تفاصيل البلاغ</h1>
-          </div>
+    if (currentIndex == 1) setCurrentIndex(2);
+    setCurrentState("تحديث الحالة الى مغلق");
+    if (currentIndex == 2) setCurrentIndex(3);
+
+    if (currentIndex == 3) console.log("dhbfh");
+  };
+
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="">
+          <h1 class="h5 text-end">البلاغات تفاصيل البلاغ</h1>
         </div>
-        <div className="row">
+      </div>
+      <div className="row">
         <div className="col-sm-12">
           <div className="m-2">
-          <div id="title"> تفاصيل البلاغ</div>
+            <div id="title"> تفاصيل البلاغ</div>
           </div>
         </div>
-    </div>
-        <div class="he shadow-sm ms-4 me-3 rounded-4">
-
-<div className="row">
-    <div className="col-sm-6 ">
-      <div className="m-2 ">
-        <div className="heading text-end pe-2">
-          الوقت والتاريخ
-        </div>
-        <hr className="hr m-0 p-2" />
-        <div className="container time  rounded p-1 mb-2 align-items-right ">
-<BsCalendar4 color='var(--primary)' className='ms-4'/>       
-     
-٢٠ اكتوبر - ١٢ مساءا
-   </div>
-        <div className="heading text-end pe-2">
-          صور المخالفة
-        </div>
-        <hr className="hr m-0 p-2" />
-        <div className="container pic rounded mb-2 shadow-sm">
-        <img src={Waste} alt="Waste" />;
-        </div>
-        <div className="heading text-end pe-2">
-          ملاحظات
-        </div>
-        <hr className="hr m-0 p-2" />
-        <div className='ps-5 ms-5 justify-content-end'>
-          <p className="h6">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem reprehenderit magni, odio eligendi laborum possimus, quaerat quos nisi, delectus sit fugiat ducimus nulla expedita libero quia totam minima eius unde!
-          </p>
+      </div>
+      <div class="he shadow-sm ms-4 me-3 rounded-4">
+        <div className="row">
+          <div className="col-sm-6 ">
+            <div className="m-2 ">
+              <div className="heading text-end pe-2">الوقت والتاريخ</div>
+              <hr className="hr m-0 p-2" />
+              <div className="container time  rounded p-1 mb-2 align-items-right ">
+                <BsCalendar4 color="var(--primary)" className="ms-4" />
+                ٢٠ اكتوبر - ١٢ مساءا
+              </div>
+              <div className="heading text-end pe-2">صور المخالفة</div>
+              <hr className="hr m-0 p-2" />
+              <div className="container pic rounded mb-2 shadow-sm">
+                <img src={Waste} alt="Waste" />;
+              </div>
+              <div className="heading text-end pe-2">ملاحظات</div>
+              <hr className="hr m-0 p-2" />
+              <div className="ps-5 ms-5 justify-content-end">
+                <p className="h6">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Autem reprehenderit magni, odio eligendi laborum possimus,
+                  quaerat quos nisi, delectus sit fugiat ducimus nulla expedita
+                  libero quia totam minima eius unde!
+                </p>
+              </div>
+            </div>
           </div>
+          <div className="col-sm-6">
+            <div className="m-2">
+              <div className="heading text-end pe-2">حالة البلاغ</div>
+              <hr className="hr m-0 p-2" />
+              <div className="container status rounded p-1  d-flex justify-content-center mb-2">
+                قيد المراجعة
+              </div>
+              <div className="heading text-end pe-2">موقع المخالفة</div>
+              <hr className="hr m-0 p-2" />
+              <div className="container pic rounded shadow-sm mb-5"></div>
+              <div className="row"> </div>
+              <div className="container mt-5 pt-5">
+                <div className="row">
+                  <div className="col-6">
+                    <Button
+                      variant="secondary"
+                      size="lg"
+                      className="send btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#myModal"
+                    >
+                      تحديث حالة البلاغ
+                    </Button>
+                  </div>
+                  <div className="col-6">
+                    <Button
+                      variant="secondary"
+                      size="lg"
+                      className="cancel btn"
+                    >
+                      {" "}
+                      إلغاء{" "}
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div>
+        <div className="modal" id="myModal">
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="">
+                <div className="row align-items-center  justify-content-end mb-4 pt-2">
+                  <div className="col-6 p-0 ">
+                    <h4 className=" m-0">تحديث الحالة</h4>
+                  </div>
+                  <div className="col-2">
+                    <button
+                      data-bs-dismiss="modal"
+                      className="closebtn btn rounded"
+                    >
+                      &#x2715;
+                    </button>
+                  </div>
+                </div>
+                <div className="modal-body justify-content-center">
+                  <div className="row align-items-center  justify-content-center">
+                    <div className="col-8 progressbar  pb-4">
+                      <MultiStepProgressBar currentStep={currentIndex} />
+                    </div>
+                    <div className="row align-items-center justify-content-between pb-4 me-4 pt-2">
+                      <div className="col-3 align-items-center h6">
+                        قيد الإنتظار
+                      </div>
+                      <div className="col-3 h6">قيد المراجعة</div>
+                      <div className="col-3 h6">مغلق</div>
+                    </div>
+                    <div className="row justify-content-start align-items-start">
+                      <div className="col-8 h5">
+                        {check()}
+                        {currentState}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div></div>
+
+              <div className="modal-footer border border-0 justify-content-center">
+                <Button
+                  variant="secondary"
+                  size="md"
+                  className="updatS btn"
+                  onClick={update}
+                >
+                  {" "}
+                  تحديث الحالة
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div className="col-sm-6">
-      <div className="m-2">
-        <div className="heading text-end pe-2">
-          حالة البلاغ
-        </div>
-        <hr className="hr m-0 p-2" />
-        <div className="container status rounded p-1  d-flex justify-content-center mb-2">
-          قيد المراجعة
-        </div>
-        <div className="heading text-end pe-2">
-          موقع المخالفة
-        </div>
-        <hr className="hr m-0 p-2" />
-        <div className="container pic rounded shadow-sm mb-5">
-       
-        </div> 
-        <div className="row"> </div>
-        <div className="container mt-5 pt-5">
-          <div className="row">
-            <div className="col-6">
-            <Button variant="secondary" size="lg" className="send btn" data-bs-toggle="modal" data-bs-target="#myModal">تحديث حالة البلاغ</Button>
-            </div>
-            <div className="col-6">
-             
-              <Button variant="secondary" size="lg" className="cancel btn">  إلغاء </Button>
-            </div>
-            </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
-</div>
-   
-      
-<div>
-
-
-<div className="modal" id="myModal">
-  <div className="modal-dialog modal-dialog-centered">
-    <div className="modal-content">
-
-    
-      <div className="">
-        <div className="row align-items-center  justify-content-end mb-4 pt-2">
-          <div className="col-6 p-0 ">
-        <h4 className=' m-0'>تحديث الحالة</h4>
-        </div>
-        <div className="col-2">
-        <button data-bs-dismiss="modal" className='closebtn btn rounded'>
-
-        &#x2715;
-       </button>
-       </div>
-       </div>
-       <div className="modal-body justify-content-center">
-       <div className="row align-items-center  justify-content-center">
-       <div className='col-8 progressbar  pb-4'>
-      <MultiStepProgressBar currentStep={currentIndex}/>
-       </div>
-       <div className="row align-items-center justify-content-between pb-4 me-4 pt-2">
-        <div className="col-3 align-items-center h6">
-          قيد الإنتظار
-        </div>
-        <div className="col-3 h6">
-          قيد المراجعة
-        </div>
-        <div className="col-3 h6">
-           مغلق
-        </div>
-       </div>
-       <div className="row justify-content-start align-items-start">
-        <div className="col-8 h5">
-       
-          {check()}
-       {currentState}
-        </div>
-       </div>
-      
-       </div>
-       </div>
-        </div>
-      
-      <div >
-      </div>
-
-
- 
-      <div className="modal-footer border border-0 justify-content-center">
-      <Button variant="secondary" size="md" className="updatS btn" onClick={update}>  تحديث الحالة</Button>
-
-
-      </div>
-</div>
-    </div>
-  </div>
-</div>
-</div>
-        );
-
-
+  );
 }
 
 export default UpdateStatus;
