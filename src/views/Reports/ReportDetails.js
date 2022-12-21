9f8474a918068b9c59b2088061b58efb906dc0bb
mahanuaim <Maha.alnuaim2@gmail.com>
2022-12-21 16:17:22 +0300

    fixing ui

--- src/views/Reports/ReportDetails.js
+++ src/views/Reports/ReportDetails.js
@@ -3,12 +3,42 @@ import Waste from '../../assets/images/waste.png';
 import Button from "react-bootstrap/Button"
 import { BsCalendar4 } from "react-icons/bs";
 import { MdOutlineModeEditOutline } from "react-icons/md";
-
+import {
+  GoogleMap,
+  Marker,
+  useJsApiLoader,
+  MarkerF,
+} from "@react-google-maps/api";
 
 import React from "react";
 
 
 function ReportDetails() {
+  const containerStyle = {
+  width: "100%;",
+  height: "100%",
+
+};
+  const center = {
+    lat: 24.72,
+    lng: 46.62,
+  };
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDvPoFbe6MDqYRGifizC34rXPlgGzCd9sE",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
   return (
 
     <div className="App">
@@ -19,21 +49,21 @@ function ReportDetails() {
         </div>
         <div className="row">
         <div className="col-sm-12">
-          <div className="m-2">
+          <div className="m-2 mt-0">
           <div id="title"> تفاصيل البلاغ</div>
           </div>
         </div>
     </div>
-        <div class="he shadow-sm ms-4 me-3 rounded-4">
+        <div class="he shadow-sm ms-4 me-3 rounded-4 pb-0">
 
 <div className="row">
     <div className="col-sm-6 ">
-      <div className="m-2 ">
+      <div className="m-2 mt-0">
         <div className="heading text-end pe-2">
           الوقت والتاريخ
         </div>
         <hr className="hr m-0 p-2" />
-        <div className="container time  rounded p-1 mb-2 align-items-right ">
+        <div className="container time  rounded p-1 mb-4 align-items-right ">
 <BsCalendar4 color='var(--primary)' className='ms-4'/>       
      
 ٢٠ اكتوبر - ١٢ مساءا
@@ -42,7 +72,7 @@ function ReportDetails() {
           صور المخالفة
         </div>
         <hr className="hr m-0 p-2" />
-        <div className="container pic rounded mb-2 shadow-sm">
+        <div className="container pic rounded mb-4 shadow-sm">
         <img src={Waste} alt="Waste" />;
         </div>
         <div className="heading text-end pe-2">
@@ -50,28 +80,40 @@ function ReportDetails() {
         </div>
         <hr className="hr m-0 p-2" />
         <div className='ps-5 ms-5 justify-content-end'>
-          <p className="h6">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem reprehenderit magni, odio eligendi laborum possimus, quaerat quos nisi, delectus sit fugiat ducimus nulla expedita libero quia totam minima eius unde!
+          <p className="h6 ps-5">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Autem reprehenderit magni, odio eligendi laborum possimus,
+                  quaerat quos nisi, delectus sit fugiat !
           </p>
           </div>
 
       </div>
     </div>
     <div className="col-sm-6">
-      <div className="m-2">
+      <div className="m-2 mt-0">
         <div className="heading text-end pe-2">
           حالة البلاغ
         </div>
         <hr className="hr m-0 p-2" />
-        <div className="container status rounded p-1  d-flex justify-content-center mb-2">
+        <div className="container status rounded p-1  d-flex justify-content-center mb-4">
           قيد المراجعة
         </div>
         <div className="heading text-end pe-2">
           موقع المخالفة
         </div>
         <hr className="hr m-0 p-2" />
-        <div className="container pic rounded shadow-sm mb-5">
-       
+        <div className="container loc rounded mb-5 shadow-sm p-0">
+        {isLoaded ? (
+            <GoogleMap
+            mapContainerStyle={containerStyle}
+              center={center}
+              zoom={7}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+            ></GoogleMap>
+          ) : (
+            <div>Loading...</div>
+          )}
         </div> 
         <div className="row"> </div>
         <div className="container mt-5 pt-5">
