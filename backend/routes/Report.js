6e9c04866917b0a2ce82eea7ee73afa4534a83ed
mahanuaim <Maha.alnuaim2@gmail.com>
2022-12-26 13:57:33 +0300

    backend setup-reportcollection

---
+++ backend/routes/Report.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const { createReport, getReport, getReports, updateReport, deleteReport } = require('../controllers/Reportcontroller');
+
+
+const  router = express.Router();
+router.get('/' ,getReports);
+router.get('/:id' ,getReport);
+router.post('/' ,createReport);
+router.delete('/:id' ,deleteReport);
+router.patch('/:id' ,updateReport);
+
+module.exports =router
+
+
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const url = `mongodb+srv://Reem:aC42mnneZgxPksaf@cluster0.cgkke2f.mongodb.net/?retryWrites=true&w=majority`;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const ReportRoutes  = require('./routes/Report')
+
+const connection_string = `mongodb+srv://Nailah:0UUXTrBNuiKanCcC@cluster0.cgkke2f.mongodb.net/Rukam?retryWrites=true&w=majority`;
+//const port = process.env.PORT || 80
+
+
+// middlewar
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/Report', ReportRoutes)
+
+
+
+mongoose
+  .connect(connection_string)
+  .then(() => {console.log("MongoDB connection established.")
+  app.listen(4000, () => {
+    console.log("Server running on port 4000.");
+  });})
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
