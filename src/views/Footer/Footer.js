ee0269a04ca63ddeae611b02b9fa4d207d394a68
Nailah-Alqahtani <NailahSMQ@gmail.com>
2022-12-26 20:50:00 +0300

    login

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -13,45 +13,43 @@ function Footer() {
   return (
     <div className=" footerimg p-lg-5  p-sm-0 pb-md-2  text-white">
       {/* <div className="container m-4 "> */}
-        <div className="row   align-items-end">
+      <div className="row   align-items-end">
+        <div className="col-1 ">
+          <img
+            className="position-absolute bottom-0 end-0 m-3"
+            width={110}
+            src={Saudi}
+            alt=""
+          />
+        </div>
+        <div className="col-2 align-middle me-5 ">
+          <ul class="">
+            <li className="list-group-item fontBold pb-2">القائمة</li>
+            <li class="list-group-item ">الصفحة الرئيسية</li>
+            <li class="list-group-item">من نحن؟</li>
+          </ul>
+        </div>
+        <div className="col-2 align-middle">
+          <ul class="">
+            <li className="list-group-item fontBold pb-2">الدعم</li>
+            <li class="list-group-item ">سياسة الخصوصية</li>
+            <li class="list-group-item  ">الدعم الفني</li>
+          </ul>
+        </div>
+        <div className="col-2 align-middle">
+          <ul class="">
+            <li className="list-group-item fontBold pb-2">تواصل معنا</li>
+            <li class="list-group-item ">الرقم المباشر ٣١٩</li>
+            <li class="list-group-item  ">العناية بالعملاء</li>
+          </ul>
+        </div>
+        <div className="col-1 mx-2"></div>
 
-
-          <div className="col-1 ">
-            <img
-              className="position-absolute bottom-0 end-0 m-3"
-              width={110}
-              src={Saudi}
-              alt=""
-            />
-          </div>
-          <div className="col-2 align-middle me-5 ">
-            <ul class="">
-              <li className="list-group-item fontBold pb-2">القائمة</li>
-              <li class="list-group-item ">الصفحة الرئيسية</li>
-              <li class="list-group-item">من نحن؟</li>
-            </ul>
-          </div>
-          <div className="col-2 align-middle">
-            <ul class="">
-              <li className="list-group-item fontBold pb-2">الدعم</li>
-              <li class="list-group-item ">سياسة الخصوصية</li>
-              <li class="list-group-item  ">الدعم الفني</li>
-            </ul>
-          </div>
-          <div className="col-2 align-middle">
-            <ul class="">
-              <li className="list-group-item fontBold pb-2">تواصل معنا</li>
-              <li class="list-group-item ">الرقم المباشر ٣١٩</li>
-              <li class="list-group-item  ">العناية بالعملاء</li>
-            </ul>
-          </div>
-          <div className="col-1 mx-2"></div>
-
-          <div id='copy-right' className=" col-3 text-white mt-5 pt-1">
-            جميع الحقوق محفوظة لــركام &copy; {new Date().getFullYear()}
-      </div>
+        <div id="copy-right" className=" col-3 text-white mt-5 pt-1">
+          جميع الحقوق محفوظة لــركام &copy; {new Date().getFullYear()}
         </div>
       </div>
+    </div>
     // </div>
   );
 }
