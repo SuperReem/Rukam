8e28363084c555af9edb22f2513cc00a8b136da4
Nailah-Alqahtani <NailahSMQ@gmail.com>
2023-01-01 22:25:05 +0300

    report list

--- src/components/Reports/Report_card.js
+++ src/components/Reports/Report_card.js
@@ -50,113 +50,111 @@ function ReportCard({ report }) {
     <>
       {index == 0 ? (
         <>
-      <div className="report-card-container">
-        <div className="report-card">
-          <div className={"report-status-container " + report.status}>
-            <h6>
-              {report.status == "unsent"
-                ? "غير مرسل"
-                : report.status == "pending"
-                ? "قيد الإنتظار"
-                : report.status == "under_processing"
-                ? "قيد المراجعة"
-                : "مغلق"}
-            </h6>
-          </div>
-          <div
-            className="report-card-content d-flex flex-column flex-md-row justify-content-between align-items-end pt-3"
-            id="report-info"
-          >
-            <div className="d-flex">
-              <h5 className="ms-5"> {report.region}</h5>
-              <p className="report-time">
-                {" "}
-                <TimeAgo date={report.createdAt} formatter={formatter} />;
-              </p>
-            </div>
-            <div>
-              <Button
-            
-                id="details-button"
-              
-                onClick={() => {
-
-        
-                  setIndex(1)
-                }
-                 }
+          <div className="report-card-container">
+            <div className="report-card">
+              <div className={"report-status-container " + report.status}>
+                <h6>
+                  {report.status == "unsent"
+                    ? "غير مرسل"
+                    : report.status == "pending"
+                    ? "قيد الإنتظار"
+                    : report.status == "under_processing"
+                    ? "قيد المراجعة"
+                    : "مغلق"}
+                </h6>
+              </div>
+              <div
+                className="report-card-content d-flex flex-column flex-md-row justify-content-between align-items-end pt-3"
+                id="report-info"
               >
-                <BsArrowUpLeft /> التفاصيل
-              </Button>
-              <Button
-                variant="secondary"
-                size="md"
-                id="delete-report-button"
-                data-bs-toggle="modal"
-                data-bs-target="#myModal"
-              >
-                <FiTrash /> حذف البلاغ
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="modal" id="myModal">
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="">
-                <div className="row align-items-center  justify-content-end mb-4 pt-2">
-                  <div className="col-6 p-0 ">
-                    <h4 className=" m-3">حذف البلاغ</h4>
-                  </div>
-                  <div className="col-2">
-                    <button
-                      data-bs-dismiss="modal"
-                      className="closebtn btn rounded"
-                    >
-                      &#x2715;
-                    </button>
-                  </div>
+                <div className="d-flex">
+                  <h5 className="ms-5"> {report.region}</h5>
+                  <p className="report-time">
+                    {" "}
+                    <TimeAgo date={report.createdAt} formatter={formatter} />;
+                  </p>
                 </div>
-                <div className="modal-body justify-content-center">
-                  <div className="row align-items-center  justify-content-center">
-                    <div className="col-8 progressbar  pb-4"></div>
-                    <div className="row align-items-center justify-content-between pb-4 me-5 pt-2">
-                      <div className="justify-content-center me-3 h4">
-                        هل انت متأكد من حذف هذا البلاغ؟
+                <div>
+                  <Button
+                    id="details-button"
+                    onClick={() => {
+                      setIndex(1);
+                    }}
+                  >
+                    <BsArrowUpLeft /> التفاصيل
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    size="md"
+                    id="delete-report-button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#myModal"
+                  >
+                    <FiTrash /> حذف البلاغ
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="modal" id="myModal">
+              <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                  <div className="">
+                    <div className="row align-items-center  justify-content-end mb-4 pt-2">
+                      <div className="col-6 p-0 ">
+                        <h4 className=" m-3">حذف البلاغ</h4>
+                      </div>
+                      <div className="col-2">
+                        <button
+                          data-bs-dismiss="modal"
+                          className="closebtn btn rounded"
+                        >
+                          &#x2715;
+                        </button>
                       </div>
                     </div>
-                    <div className="row justify-content-start align-items-start">
-                      <div className="col-8 h5"></div>
+                    <div className="modal-body justify-content-center">
+                      <div className="row align-items-center  justify-content-center">
+                        <div className="col-8 progressbar  pb-4"></div>
+                        <div className="row align-items-center justify-content-between pb-4 me-5 pt-2">
+                          <div className="justify-content-center me-3 h4">
+                            هل انت متأكد من حذف هذا البلاغ؟
+                          </div>
+                        </div>
+                        <div className="row justify-content-start align-items-start">
+                          <div className="col-8 h5"></div>
+                        </div>
+                      </div>
                     </div>
+                  </div>
+
+                  <div></div>
+
+                  <div className="modal-footer border border-0 justify-content-center">
+                    <Button
+                      variant="secondary"
+                      size="md"
+                      id="delete-report-button"
+                    >
+                      {" "}
+                      حذف{" "}
+                    </Button>
+                    <Button variant="secondary" size="md" id="details-button">
+                      {" "}
+                      إلغاء{" "}
+                    </Button>
                   </div>
                 </div>
               </div>
-
-              <div></div>
-
-              <div className="modal-footer border border-0 justify-content-center">
-                <Button variant="secondary" size="md" id="delete-report-button">
-                  {" "}
-                  حذف{" "}
-                </Button>
-                <Button variant="secondary" size="md" id="details-button">
-                  {" "}
-                  إلغاء{" "}
-                </Button>
-              </div>
             </div>
           </div>
-        </div>
-      </div>
-      </>
+        </>
       ) : (
         <ReportDetails report={report} />
       )}
     </>
   );
 }
-
 
 export default ReportCard;
--- src/views/Reports/Reports_list.js
+++ src/views/Reports/Reports_list.js
@@ -10,6 +10,14 @@ import Pagination from "@mui/material/Pagination";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useReportContext } from "../../hooks/useReportContext";
 import { useEffect } from "react";
+import "../../components/Reports/report_card.css";
+import Button from "react-bootstrap/Button";
+import { BsArrowUpLeft } from "react-icons/bs";
+import { FiTrash } from "react-icons/fi";
+import TimeAgo from "react-timeago";
+import frenchStrings from "react-timeago/lib/language-strings/ar";
+import buildFormatter from "react-timeago/lib/formatters/buildFormatter";
+import ReportDetails from "../../views/Reports/ReportDetails";
 
 const theme = createTheme({
   status: {
@@ -27,6 +35,9 @@ const theme = createTheme({
   },
 });
 function ReportsList() {
+  const formatter = buildFormatter(frenchStrings);
+  const [index, setIndex] = useState(0);
+  const [rep, setReport] = useState();
   const { reports, dispatch } = useReportContext();
 
   useEffect(() => {
@@ -50,40 +61,160 @@ function ReportsList() {
   const datePickerRef = useRef();
   return (
     <>
-      <div id="title"> البلاغات </div>
-      <div id="filter">
-        <BsFilter
-          size={43}
-          onClick={() => datePickerRef.current.openCalendar()}
-        />{" "}
-        <DatePicker
-          ref={datePickerRef}
-          locale={arabic_ar}
-          range="true"
-          className="green"
-          inputClass="custom-input"
-          maxDate={new Date()}
-        />
-      </div>
-      {reports &&
-        reports.map((report) => (
-          <ReportCard report={report} key={report._id} />
-        ))}
-      {/* <ReportCard status={"unsent"} />
-      <ReportCard status={"pending"} />
-      <ReportCard status={"closed"} /> */}
-      <div id="pagination">
-        <ThemeProvider theme={theme}>
-          <Pagination
-            count={10}
-            variant="outlined"
-            size="large"
-            color="primary"
-          />
-        </ThemeProvider>
-      </div>
+      {index == 0 ? (
+        <>
+          <div id="title"> البلاغات </div>
+          <div id="filter">
+            <BsFilter
+              size={43}
+              onClick={() => datePickerRef.current.openCalendar()}
+            />{" "}
+            <DatePicker
+              ref={datePickerRef}
+              locale={arabic_ar}
+              range="true"
+              className="green"
+              inputClass="custom-input"
+              maxDate={new Date()}
+            />
+          </div>
+          {reports &&
+            reports.map((report) => (
+              <>
+                <div className="report-card-container">
+                  <div className="report-card">
+                    <div className={"report-status-container " + report.status}>
+                      <h6>
+                        {report.status == "unsent"
+                          ? "غير مرسل"
+                          : report.status == "pending"
+                          ? "قيد الإنتظار"
+                          : report.status == "under_processing"
+                          ? "قيد المراجعة"
+                          : "مغلق"}
+                      </h6>
+                    </div>
+                    <div
+                      className="report-card-content d-flex flex-column flex-md-row justify-content-between align-items-end pt-3"
+                      id="report-info"
+                    >
+                      <div className="d-flex">
+                        <h5 className="ms-5"> {report.region}</h5>
+                        <p className="report-time">
+                          {" "}
+                          <TimeAgo
+                            date={report.createdAt}
+                            formatter={formatter}
+                          />
+                          ;
+                        </p>
+                      </div>
+                      <div>
+                        {/* <Button
+                          id="details-button"
+                          onClick={() => {
+                            setIndex(1);
+                          }}
+                        >
+                          <BsArrowUpLeft /> التفاصيل
+                        </Button> */}
+                        <button
+                          onClick={() => {
+                            setReport(report);
+                            setIndex(1);
+                          }}
+                        >
+                          Click
+                        </button>
+                        <Button
+                          variant="secondary"
+                          size="md"
+                          id="delete-report-button"
+                          data-bs-toggle="modal"
+                          data-bs-target="#myModal"
+                        >
+                          <FiTrash /> حذف البلاغ
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="modal" id="myModal">
+                    <div className="modal-dialog modal-dialog-centered">
+                      <div className="modal-content">
+                        <div className="">
+                          <div className="row align-items-center  justify-content-end mb-4 pt-2">
+                            <div className="col-6 p-0 ">
+                              <h4 className=" m-3">حذف البلاغ</h4>
+                            </div>
+                            <div className="col-2">
+                              <button
+                                data-bs-dismiss="modal"
+                                className="closebtn btn rounded"
+                              >
+                                &#x2715;
+                              </button>
+                            </div>
+                          </div>
+                          <div className="modal-body justify-content-center">
+                            <div className="row align-items-center  justify-content-center">
+                              <div className="col-8 progressbar  pb-4"></div>
+                              <div className="row align-items-center justify-content-between pb-4 me-5 pt-2">
+                                <div className="justify-content-center me-3 h4">
+                                  هل انت متأكد من حذف هذا البلاغ؟
+                                </div>
+                              </div>
+                              <div className="row justify-content-start align-items-start">
+                                <div className="col-8 h5"></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
 
-      <div id="page-number2">1-20 صفحة</div>
+                        <div></div>
+
+                        <div className="modal-footer border border-0 justify-content-center">
+                          <Button
+                            variant="secondary"
+                            size="md"
+                            id="delete-report-button"
+                          >
+                            {" "}
+                            حذف{" "}
+                          </Button>
+                          <Button
+                            variant="secondary"
+                            size="md"
+                            id="details-button"
+                          >
+                            {" "}
+                            إلغاء{" "}
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ))}
+
+          <div id="pagination">
+            <ThemeProvider theme={theme}>
+              <Pagination
+                count={10}
+                variant="outlined"
+                size="large"
+                color="primary"
+              />
+            </ThemeProvider>
+          </div>
+
+          <div id="page-number2">1-20 صفحة</div>
+        </>
+      ) : (
+        <ReportDetails report={rep} />
+      )}
     </>
   );
 }
