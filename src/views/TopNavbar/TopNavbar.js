8aef1c064511c9e1d6e581e47fb5ee208d9880b8
deemmf <88791711+deemmf@users.noreply.github.com>
2023-02-02 21:13:59 +0300

    email

--- src/views/TopNavbar/TopNavbar.js
+++ src/views/TopNavbar/TopNavbar.js
@@ -1,40 +1,17 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
 import React, { useState } from "react";
 import Logo from "../../assets/images/Logo_Light.png";
-import {
-  Link,
-  useLocation,
-  useNavigate,
-  Navigate,
-  Outlet,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function TopNavbar() {
   const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <>
-      {/*      
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar> */}
-
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         {/* <a class="navbar-brand" href="#">Navbar</a> */}
-        <a class="navbar-brand" href="#">
+        <Link to="/homePage" className="nav-link">
           <img src={Logo} width="100" alt="" />
-        </a>
-        {/* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon">hh</span>
-  </button> */}
+        </Link>
+
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav navbar-brand">
             <li onClick={() => setCurrentIndex(0)} class="nav-item active">
@@ -43,24 +20,20 @@ function TopNavbar() {
               </Link>
             </li>
             <li onClick={() => setCurrentIndex(1)} class="nav-item">
-            <Link to="/homePage" className="nav-link">
-            الخدمات
+              <Link to="/homePage" className="nav-link">
+                الخدمات
               </Link>
             </li>
             <li onClick={() => setCurrentIndex(2)} class="nav-item">
-              <a class="nav-link" href="#Login">
-                تواصل معنا
+              <a
+                href="mailto:rukamservice@gmail.com"
+                className="text-decoration-none nav-link"
+              >
+                تواصل معنا{" "}
               </a>
             </li>
           </ul>
         </div>
-        {/* <main className="page-container">
-      <div className="background fs-1">
-         {currentIndex === 0? <Login/> : currentIndex === 1? <Login/> : currentIndex === 2? <Login/> : <Login/>}
-      </div>
-    
-    </main>
-     */}
       </nav>
     </>
   );
