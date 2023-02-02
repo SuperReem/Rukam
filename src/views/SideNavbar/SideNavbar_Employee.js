814da2c2093531d43ed9813463d94c80d1c84497
mahanuaim <Maha.alnuaim2@gmail.com>
2023-02-02 20:54:41 +0300

    nav page -update

--- src/views/SideNavbar/SideNavbar_Employee.js
+++ src/views/SideNavbar/SideNavbar_Employee.js
@@ -47,10 +47,22 @@ const SidebarEmployee = () => {
           <div className="nav-list-container">
             <ul className="nav-list">
               <li onClick={() => setCurrentIndex(0)}>
+              {currentIndex == 0 ? (
+              <div className="cc d-flex align-items-center ">
                 <RiHome6Line className="sidenav-icons" /> الرئيسية
+                </div>
+              ):  <div className=" d-flex align-items-center ">
+              <RiHome6Line className="sidenav-icons" /> الرئيسية
+              </div>}
               </li>
               <li onClick={() => setCurrentIndex(1)}>
+              {currentIndex == 1 ? (
+                 <div className="cc d-flex align-items-center ">
                 <TbReportAnalytics className="sidenav-icons" /> البلاغات
+                </div>):
+                  <div className=" d-flex align-items-center ">
+                  <TbReportAnalytics className="sidenav-icons" /> البلاغات
+                  </div>}
               </li>
 
               <br></br>
