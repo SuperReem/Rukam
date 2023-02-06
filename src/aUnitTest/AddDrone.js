b7b625e16a31bd31c3c719702f7f89b850d6a698
deemmf <88791711+deemmf@users.noreply.github.com>
2023-02-06 18:32:43 +0300

    unit ++

---
+++ src/aUnitTest/AddDrone.js
@@ -0,0 +1,23 @@
+const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+const ex = /[&\/[$\]\\#,;@!+()$~%.'":*?<>{}]/g;
+export const handleSubmit =  (droneName ="") => {
+    if(droneName.trim().replace(" ","").length==0){ //if empty oronly ws
+                return false;
+            }else if (droneName.length < 2) {
+                return false; }
+             else if (droneName.length >= 2) {
+                if (droneName.length >10) {
+                    return false;
+                }if( ex.test(droneName)){
+                    return false;
+                }else{
+                    return true;} } 
+};
+
+export const handelRegion =  (region ="") => {
+    if(region!="حطين"&&region!="النخيل"&&region!="عرقه"){
+                return false;
+            }else{
+                 return true;} 
+};
+
